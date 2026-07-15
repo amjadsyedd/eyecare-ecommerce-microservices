bdd37b877ba33cec2f0e85cdf07234740b65492e
amjadsyedd <syedamjad8820@gmail.com>
2026-07-15 21:37:08 +0530

    Sprint 2: Added Eureka Service Registry

---
+++ serviceregistry/src/test/java/com/eyecare/serviceregistry/ServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyecare.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
